a8c53efc104636f240a91c1d6728f256d25f1e73
Sourabh Sethi <sourabhhsethii@linux.com>
2022-02-15 16:51:34 -0500

    Validation Added using validator

--- src/main/java/com/upgrad/exceptiondemo/controller/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/upgrad/exceptiondemo/controller/advice/GlobalExceptionAdvice.java
@@ -4,9 +4,11 @@ import com.upgrad.exceptiondemo.exceptions.RequestedResourceNotFoundException;
 import com.upgrad.exceptiondemo.model.ErrorModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice {
@@ -17,6 +19,16 @@ public class GlobalExceptionAdvice {
                 .builder()
                 .errorCode("ERR_RESOURCE_NOT_FOUND")
                 .errorMessage("Resource Not Found")
+                .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ErrorModel> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        return new ResponseEntity(ErrorModel
+                .builder()
+                .errorCode("ERR_INVALID_PAYLOAD_001")
+                .errorMessage("INVALID_PAYLOAD")
+                .errorFieldsList(e.getBindingResult().getFieldErrors().stream().map(fe-> fe.getDefaultMessage()).collect(Collectors.toList()))
                 .build(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/upgrad/exceptiondemo/model/Student.java
+++ src/main/java/com/upgrad/exceptiondemo/model/Student.java
@@ -3,14 +3,24 @@ package com.upgrad.exceptiondemo.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @Data
 @Builder
 public class Student {
 
     private String studentId;
+
+    @NotBlank(message = "Student Name cannot be blank")
+    @Size(min = 5, max = 20, message = "Student Name length should be between 5 and 20 Characters")
     private String studentName;
+
     private String studentDob;
+
+    @NotBlank(message = "Mobile number cannot be Black")
     private String mobile;
 
 }
